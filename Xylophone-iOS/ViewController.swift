0a7878769be260db922c28dc17b4b167533673a6
Michel Kim <kim98.michel@gmail.com>
2023-06-21 23:47:05 -0300

    Add Sounds

--- Xylophone-iOS/ViewController.swift
+++ Xylophone-iOS/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("I was pressed!")
+    }
+    
 }
 
